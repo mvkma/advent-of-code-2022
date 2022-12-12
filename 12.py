dfa7d43fb2547e2a483247254406794747adf961
Matthias Volk <mv@infrablue.org>
2022-12-12 17:00:11 +0100

    Day 12 cleanup

--- 12.py
+++ 12.py
@@ -2,107 +2,97 @@ from collections import namedtuple, defaultdict
 
 INPUT_FILE = "input_12"
 
-# class Point:
-#     def __init__(self, x1=0, x2=0):
-#         self.x1 = x1
-#         self.x2 = x2
-# 
-#     def __repr__(self):
-#         return f"Point({self.x1}, {self.x2})"
-# 
-#     def __hash__(self):
-#         return hash((self.x1, self.x2))
-# 
-#     def __eq__(self, other):
-#         assert isinstance(other, Point)
-#         return (self.x1 == other.x1) and (self.x2 == other.x2)
-# 
-#     def __add__(self, other):
-#         assert isinstance(other, Point)
-#         return Point(self.x1 + other.x1, self.x2 + other.x2)
-
-Point = namedtuple("Point", ["x1", "x2"])
-
-DIRECTIONS = {
-    "R": Point( 1,  0),
-    "L": Point(-1,  0),
-    "U": Point( 0,  1),
-    "D": Point( 0, -1),
-}
-
 SAMPLE = """Sabqponm
 abcryxxl
 accszExk
 acctuvwj
 abdefghi"""
 
-heightmap = []
+GRID_DIRECTIONS = ((1, 0), (-1, 0), (0, 1), (0, -1))
 
-with open(INPUT_FILE) as f:
-    # for i, line in enumerate(SAMPLE.splitlines()):
+def parse_heightmap(f):
+    heightmap = []
+    start = None
+    end = None
+
     for i, line in enumerate(f):
-        line = line.strip()
-
         row = []
-        for j, c in enumerate(line):
+        for j, c in enumerate(line.strip()):
             if c == "S":
-                start = Point(j, i)
+                start = (i, j)
                 c = "a"
             elif c == "E":
-                end = Point(j, i)
+                end = (i, j)
                 c = "z"
 
-            row.append(ord(c) - 97)
+            row.append(ord(c) - ord("a"))
 
         heightmap.append(row)
+        
+    return heightmap, start, end
 
+def find_shortest_path(heightmap, start, end, verbose_freq=100):
+    # We keep a dictionary that maps point (x, y) -> path
+    # where path is the shortest path we have found to (x, y) up until now.
+    paths = dict()
+    paths[start] = [start]
 
-nrows = len(heightmap)
-ncols = len(heightmap[0])
-finished = []
+    nrows = len(heightmap)
+    ncols = len(heightmap[0])
 
-apoints = []
-for i in range(nrows):
-    for j in range(ncols):
-        if heightmap[i][j] == 0:
-            apoints.append(Point(j, i))
+    k = 0
 
-k = 0
+    while True:
+        points = tuple(paths.keys())
 
-cur = end
-paths = defaultdict(list)
-paths[cur] = [cur]
+        if end in points:
+            break
 
-while True:
-    # print({k: len(v) for k, v in paths.items()})
-    # print(len(paths[end]))
+        if (k % verbose_freq) == 0:
+            print(k, len(points))
 
-    points = tuple(paths.keys())
-    # if start in points:
-    #     break
-    if all(p in points for p in apoints):
-        break
- 
-    if (k % 100) == 0:
-        print(k, len(points))
+        for x0, x1 in points:
+            for d1, d2 in GRID_DIRECTIONS:
+                new_x0 = x0 + d1
+                new_x1 = x1 + d2
 
-    for pt in points:
-        height = heightmap[pt.x2][pt.x1]
-        path = paths[pt]
-
-        for d1, d2 in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-            if (not (0 <= pt.x1 + d1 < ncols)) or (not (0 <= pt.x2 + d2 < nrows)):
-                continue
- 
-            # if (heightmap[pt.x2 + d2][pt.x1 + d1] - height) <= 1:
-            if (height - heightmap[pt.x2 + d2][pt.x1 + d1]) <= 1:
-                new_pt = Point(pt.x1 + d1, pt.x2 + d2)
-
-                if new_pt in path:
+                # We're at the edge of the grid
+                if (not (0 <= new_x0 < nrows)) or (not (0 <= new_x1 < ncols)):
                     continue
 
-                if len(paths[new_pt]) == 0 or len(paths[new_pt]) > (len(path) + 1):
-                    paths[new_pt] = path + [new_pt]
+                # We are allowed to go one step down and arbitrary many steps up in this way
+                # (reversed compared to the description in the problem)
+                if (heightmap[x0][x1] - heightmap[new_x0][new_x1]) <= 1:
+                    cur_path = paths[(x0, x1)]
+                    new_pt = (new_x0, new_x1)
 
-    k += 1
-            
+                    if new_pt in cur_path:
+                        continue
+
+                    if (not new_pt in paths) or (len(paths[new_pt]) > len(cur_path) + 1):
+                        paths[new_pt] = cur_path + [new_pt]
+
+        k += 1
+
+    return paths
+
+if __name__ == "__main__":
+    with open(INPUT_FILE) as f:
+        heightmap, start, end = parse_heightmap(f)
+
+    nrows = len(heightmap)
+    ncols = len(heightmap[0])
+
+    # We have to reverse end and start, because we want to go backwards
+    paths_part1 = find_shortest_path(heightmap, end, start)
+    print(len(paths_part1[start]) - 1) # 440
+
+    apoints = []
+    for i in range(nrows):
+        for j in range(ncols):
+            if heightmap[i][j] == 0:
+                apoints.append((i, j))
+
+    # Maybe we are lucky that the shortest one to an arbitrary a was already found in round 1
+    print(min([len(paths_part1[p]) for p in filter(lambda p: p in paths_part1, apoints)]) - 1)
+
