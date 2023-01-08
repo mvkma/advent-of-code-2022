389c6bb9aa8329e5e989a74bb75f50f9dfe20ef4
Matthias Volk <mv@infrablue.org>
2023-01-08 23:11:44 +0100

    Fix day 11

--- 11.py
+++ 11.py
@@ -1,26 +1,32 @@
+import re
+from functools import reduce
 from collections import defaultdict
 
 INPUT_FILE = "input_11"
 
 class Monkey:
-    def __init__(self, items, operation, test, targets):
+    def __init__(self, items, operation, prime, targets):
         self.items = items
+        self.prime = prime
         self.operation = operation
-        self.test = test
+        self.test = lambda k: (k % self.prime) == 0
         self.targets = targets
 
     def __repr__(self):
         return f"Monkey(items={self.items})"
 
-    def run(self):
+    def run(self, magic_num=None):
         out = defaultdict(list)
 
         while self.items:
             item = self.items[0]
             self.items.remove(item)
 
-            # item = int(self.operation(item) / 3)
-            item = self.operation(item) % 9699690
+            if magic_num is None:
+                item = int(self.operation(item) / 3)
+            else:
+                item = self.operation(item) % magic_num
+
             if self.test(item):
                 out[self.targets[0]].append(item)
             else:
@@ -31,46 +37,58 @@ class Monkey:
     def add_items(self, new_items):
         self.items.extend(new_items)
                 
-
-SAMPLE = [
-    Monkey([79, 98], lambda k: k * 19, lambda k: (k % 23) == 0, (2, 3)),
-    Monkey([54, 65, 75, 74], lambda k: k + 6, lambda k: (k % 19) == 0, (2, 0)),
-    Monkey([79, 60, 97], lambda k: k * k, lambda k: (k % 13) == 0, (1, 3)),
-    Monkey([74], lambda k: k + 3, lambda k: (k % 17) == 0, (0, 1)),
-]
-
-PUZZLE = [
-    Monkey([80], lambda k: k * 5, lambda k: (k % 2) == 0, (4, 3)),
-    Monkey([75, 83, 74], lambda k: k + 7, lambda k: (k % 7) == 0, (5, 6)),
-    Monkey([86, 67, 61, 96, 52, 63, 73], lambda k: k + 5, lambda k: (k % 3) == 0, (7, 0)),
-    Monkey([85, 83, 55, 85, 57, 70, 85, 52], lambda k: k + 8, lambda k: (k % 17) == 0, (1, 5)),
-    Monkey([67, 75, 91, 72, 89], lambda k: k + 4, lambda k: (k % 11) == 0, (3, 1)),
-    Monkey([66, 64, 68, 92, 68, 77], lambda k: k * 2, lambda k: (k % 19) == 0, (6, 2)),
-    Monkey([97, 94, 79, 88], lambda k: k * k, lambda k: (k % 5) == 0, (2, 7)),
-    Monkey([77, 85], lambda k: k + 6, lambda k: (k % 13) == 0, (4, 0)),
-]
-
-def play_round(monkeys):
+def play_round(monkeys, magic_num=None):
     inspected_items = defaultdict(int)
 
     for i, m in enumerate(monkeys):
         inspected_items[i] += len(m.items)
-        throws = m.run()
+        throws = m.run(magic_num=magic_num)
 
         for k, v in throws.items():
             monkeys[k].add_items(v)
 
     return inspected_items
 
+def parse_monkey(monkey_desc):
+    items = list(map(int, re.findall("[-]*\d+", monkey_desc[1])))
+    op = eval("lambda old:" + monkey_desc[2].split("=")[1])
+    p = int(re.findall("\d+", monkey_desc[3])[0])
+    t1 = int(re.findall("\d+", monkey_desc[4])[0])
+    t2 = int(re.findall("\d+", monkey_desc[5])[0])
+
+    return Monkey(items, op, p, (t1, t2))
+
 if __name__ == "__main__":
+    with open(INPUT_FILE) as f:
+        monkeys = []
+        while True:
+            monkey_desc = [f.readline().strip() for _ in range(7)]
+            if all(l == "" for l in monkey_desc):
+                break
+
+            monkeys.append(monkey_desc)
+
+    # Part 1
+    puzzle = [parse_monkey(desc) for desc in monkeys]
     inspected_items = defaultdict(int)
 
-    for i in range(10_000):
-        if (i % 100) == 0:
-            print(i)
-        rd = play_round(PUZZLE)
+    for i in range(20):
+        rd = play_round(puzzle)
         for k, v in rd.items():
             inspected_items[k] += v
 
     top_two = list(sorted(inspected_items.values()))[-2:]
-    print(top_two)
+    print(top_two[0] * top_two[1])
+
+    # Part 2
+    puzzle = [parse_monkey(desc) for desc in monkeys]
+    magic_num = reduce(lambda a, b: a * b, [m.prime for m in puzzle])
+    inspected_items = defaultdict(int)
+
+    for i in range(10_000):
+        rd = play_round(puzzle, magic_num=magic_num)
+        for k, v in rd.items():
+            inspected_items[k] += v
+
+    top_two = list(sorted(inspected_items.values()))[-2:]
+    print(top_two[0] * top_two[1])
